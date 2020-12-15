f080219911f217aeb579e3cd83b03d7c7e7e730e
abobakr99 <bamangar@gmail.com>
2020-12-14 19:56:15 -0500

    fixed test cases

--- tests/test_parcel_loader.py
+++ tests/test_parcel_loader.py
@@ -16,6 +16,13 @@ def setup_file(points):
     except subprocess.CalledProcessError as e:
         pytest.fail("Failled to create test file. Error: {}".format(e))
 
+def invalid_points_file_setup(points):
+    if not isinstance(points,str):
+       points = str(points)
+
+    with open(fpath, 'w') as file:
+        file.write(points)
+    
 def read_from_file():
     file1 = open(fpath, 'r') 
     Lines = file1.readlines()
@@ -35,7 +42,7 @@ def read_from_file():
     return file_content
 
 def call_process():
-    out = subprocess.check_output('./parcel_loader_v1 -f ./{}'.format(fpath), stderr=subprocess.STDOUT, shell=True).decode()
+    out = subprocess.check_output('../bins/parcel_loader_v1 -f ./{}'.format(fpath), stderr=subprocess.STDOUT, shell=True).decode()
     return out
 
 def find_match(regex,out):
@@ -65,20 +72,28 @@ def test_008_load_time(setup_test_008):
     print ('Actual result = {}\n''Expected result <={}'.format(min_time, 30.00))
     assert min_time <= 30.00, 'Minimum time should be 30 or less for point: {}'.format(points)
 
-@pytest.fixture
-def setup_test_009():
-    points = ((1,10,10),)
+@pytest.fixture(params = [((1,10,10),) , ((12,10,10),) ])
+def setup_test_009(request):
+    points = request.param
     setup_file(points)
     yield points
     os.remove(fpath)
-#rest multiple points 
+
 def test_009_load_time_format(setup_test_009):
     points = setup_test_009
     regex = r'^Minimum time = \d+\.\d\d'
+    min_time =0
+    if points == ((1,10,10),):
+        min_time = '21.00'
+    elif points == ((12,10,10),): 
+        min_time = '30.00'
+
     out = call_process()
-    min_time_format = find_match(regex,out)
-    print ('Actual pointsresult = {}\n''Expected result = {}'.format(min_time_format, 'Minimum time = 21.00'))
-    assert min_time_format == 'Minimum time = 21.00' , 'Minimum time should look like [Minimum time = 21.00] for point: {}'.format(points)
+    min_time_string = find_match(regex,out)
+
+    print ('Actual pointsresult = {}\n''Expected result = {}'.format(min_time_string, 'Minimum time = '+ min_time))
+    
+    assert min_time_string == 'Minimum time = '+ min_time, 'Minimum time should look like [Minimum time = 21.00] for point: {}'.format(points)
 
 @pytest.fixture
 def setup_test_010():
@@ -110,48 +125,29 @@ def test_011_load_time_format(setup_test_011):
     print ('Actual result = {}\n''Expected result = {}'.format(min_time_format, 'Minimum time = 21.00'))
     assert min_time_format == 'Stopping at point: x=20,y=20' , 'Minimum time should look like [Stopping at point: x=20,y=20] for point: {}'.format(points)
 
-@pytest.fixture
-def setup_test_012():
-    #(20, d, , (2,3), )
-    points = ((1,10,10),(2,10,20))
-    setup_file(points)
+@pytest.fixture(params = [ 15 , (12,10,), 'test' ])
+def setup_test_012(request):
+    points = request.param
+    invalid_points_file_setup(points)
     yield points
     os.remove(fpath)
 
-def test_012_validate_points(setup_test_012):
-    test_points = setup_test_012
-    file_content = read_from_file()
-    num_points = file_content[0]
-    points = file_content[1]
-    print ('points: == {}==\n num points === {} ==='.format(points,num_points))
-    #assert num_points == 2
-    assert num_points == len(points) ,'THe total number of points should be eqaul to the number points in the test file for {}'.format(test_points)
-
-@pytest.fixture
+def test_012_invalid_points(setup_test_012):
+    points = setup_test_012
+    regex = r'^Minimum time = (\d+\.\d\d)'
+    print('Testing points: {}'.format(points))
+    out = call_process()
+    match = find_match(regex,out)
+    min_time = float (match)
+    print ('Actual result = {}\n''Expected result ={}'.format(min_time, 20.00))
+    assert min_time == 20.00, 'Minimum time should be 20 for point: {}'.format(points)
+    
+@pytest.fixture()
 def setup_test_013():
     points = ((1,14,5),(2,5,4))
     setup_file(points)
     yield points
     os.remove(fpath)
 
-def test_013_point_foramt(setup_test_013): 
-    file_content = read_from_file()
-    num_points = file_content[0]
-    points = file_content[1]
-
-    #regex = r'[0-9]+,[0-9]|[0-9][0-9],[0-9]'
-    regex = r'\d+,\d+,\d+'
-    match = []
-    found = None
-    for p in points:
-        found = re.search(regex,p)
-        if found != None:
-            match.append(found.group())
-
-    print ('points: == {} ==\n'.format(points))
-    print ('Match: == {} ==\n'.format(match))
-
-    assert  collections.Counter(match) == collections.Counter(points), 'The point format should be 3 comma seprated values'
-
-def test_process_with_no_file():
+def test_013_process_with_no_file():
     pass
