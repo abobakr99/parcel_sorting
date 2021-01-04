cb7b20fccbbcca66b75e6ad1390d91f1fddc5f1e
abobakr99 <bamangar@gmail.com>
2021-01-03 23:05:13 -0500

    fixed bash script

--- tests/runner.sh
+++ tests/runner.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+
 # verify dependencies  exist 
+declare -a packages=(python3 pytest)
+for i in "${packages[@]}"
+do
+  command -v $i
+  if [[ $? -gt 0 ]]
+  then 
+    echo "$i need to be installed before tests can run"
+    exit
+  fi
+done
 
 # create results folder if it does not exist
 #create timestamped folder under results 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-if [[ ! -d "./results" ]]
+if [[ ! -d "../results" ]]
 then 
   echo "does not exist "
   mkdir ../results
   mkdir ../results/$timestamp
 else 
+  echo "====exists==== "
   mkdir ../results/$timestamp
-  echo "$timestamp"
 fi
 #run the tests 
 pytest test_parcel_loader.py --html=../results/$timestamp/report.html
