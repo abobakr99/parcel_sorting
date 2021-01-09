1b82b312807ef79b3de962ac403657a02d1cb4b4
abobakr99 <bamangar@gmail.com>
2021-01-08 21:03:46 -0500

    fixed bash script

--- tests/runner.sh
+++ tests/runner.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# Check if python3 is installed
+python3 --version >> /dev/null 2>&1
+if [[ $? -gt 0 ]]
+  then 
+    echo " Python3 need to be installed before tests can run"
+    exit
+  fi
 # verify dependencies  exist 
-declare -a packages=(python3 pytest)
+declare -a packages=(pytest pytest-html)
 for i in "${packages[@]}"
-do
-  command -v $i
+do 
+  pip3 show $i >> /dev/null 2>&1
   if [[ $? -gt 0 ]]
   then 
     echo "$i need to be installed before tests can run"
     exit
   fi
 done
-
 # create results folder if it does not exist
 #create timestamped folder under results 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -19,9 +25,7 @@ if [[ ! -d "../results" ]]
 then 
   echo "creating results and timestamp folders"
   mkdir ../results
-  mkdir ../results/$timestamp
-else 
-  mkdir ../results/$timestamp
 fi
+  mkdir ../results/$timestamp
 #run the tests 
 pytest test_parcel_loader.py --html=../results/$timestamp/report.html
